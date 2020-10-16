6d3eb9d6824fff5ef73bd0878ba1c086e6d5380e
bruno wandrey <bruno.wandrey@escriba.com.br>
2020-10-15 23:24:50 -0300

    left behind

--- src/main/java/wandrey/bruno/loadbalancer/controller/LoadBalancerController.java
+++ src/main/java/wandrey/bruno/loadbalancer/controller/LoadBalancerController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,27 +37,27 @@ public class LoadBalancerController {
 	ApplicationContext applicationContext;
 
 	@GetMapping
-	public <T, U> T processRequestGET(@RequestParam("service") String service, @RequestBody U body,
-			HttpServletRequest request) throws IOException {
-		return lbService.redirect(service, body, request);
+	public <T, U> ResponseEntity<T> processRequestGET(@RequestParam("service") String service,
+			@RequestParam("uri") String uri, @RequestBody U body, HttpServletRequest request) throws IOException {
+		return lbService.redirect(service, uri, body, request);
 	}
 
 	@PostMapping
-	public <T, U> T processRequestPOST(@RequestParam("service") String service, @RequestBody U body,
-			HttpServletRequest request) throws IOException {
-		return lbService.redirect(service, body, request);
+	public <T, U> ResponseEntity<T> processRequestPOST(@RequestParam("service") String service,
+			@RequestParam("uri") String uri, @RequestBody U body, HttpServletRequest request) throws IOException {
+		return lbService.redirect(service, uri, body, request);
 	}
 
 	@PutMapping
-	public <T, U> T processRequestPUT(@RequestParam("service") String service, @RequestBody U body,
-			HttpServletRequest request) throws IOException {
-		return lbService.redirect(service, body, request);
+	public <T, U> ResponseEntity<T> processRequestPUT(@RequestParam("service") String service,
+			@RequestParam("uri") String uri, @RequestBody U body, HttpServletRequest request) throws IOException {
+		return lbService.redirect(service, uri, body, request);
 	}
 
 	@DeleteMapping
-	public <T, U> T processRequestDELETE(@RequestParam("service") String service, @RequestBody U body,
-			HttpServletRequest request) throws IOException {
-		return lbService.redirect(service, body, request);
+	public <T, U> ResponseEntity<T> processRequestDELETE(@RequestParam("service") String service,
+			@RequestParam("uri") String uri, @RequestBody U body, HttpServletRequest request) throws IOException {
+		return lbService.redirect(service, uri, body, request);
 	}
 
 }
